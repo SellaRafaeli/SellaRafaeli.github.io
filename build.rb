8e070ace2de942f9b36037e9713ef3cd18178663
Sella Rafaeli <sella.rafaeli@gmail.com>
2015-12-19 23:21:54 +0200

    rebuild

--- build.rb
+++ build.rb
@@ -1,83 +1,33 @@
-# build the blog from source.
-require './helper_build'
-require 'JSON'
-
-require 'Find'
-#require 'pry'
+#$ ruby generate_content.rb
+require 'find'
 require 'kramdown'
 
-#1 build index and about pages. 
-def build_basic_pages
-  puts __method__
-  basic_pages = ['index.md', 'about.md']
-  basic_pages.each do |path|  
-    html_path = path.sub('.md','.html')              
-    make_nice_html(path, html_path)
-  end
+def wrap_file_in_html(file_path, custom_content = nil)     
+   template = "<html><head> <meta charset='utf-8'> <title>#{File.basename(file_path, ".html")} </title>"+
+                        #'<link href="http://kevinburke.bitbucket.org/markdowncss/markdown.css" rel="stylesheet"></link>'+
+                        '<link href="https://raw.githubusercontent.com/sindresorhus/github-markdown-css/gh-pages/github-markdown.css" rel="stylesheet"></link>'+
+                        '<link href="/main.css" rel="stylesheet"></link>'+
+                        "</head>"+
+                        '<body>'+
+                        '<article class="markdown-body">'+
+                        "<a href='/'>Home</a>" + "  |  " +
+                        "#{custom_content || File.read(file_path)}"+
+                        '</article>'+
+                        "</body></html>"
+   File.write(file_path,template)
 end
 
-def get_blog_mds_paths
-  puts __method__
-  paths = []
-  Find.find('blog_src') do |path| paths << path if path =~ /.*\.md$/ end
-  paths
+#2. Find all Markdown files
+md_file_paths = Find.find('.').to_a.select {|f| f =~ /.*\.md$/ }
+
+#3. Turn each of them into an HTML file with same name in same place. 
+md_file_paths.each do |path|
+  html_path = path.sub('.md','.html')
+  puts "generating #{path} into #{html_path}..."
+  text = File.read(path)
+  File.write(html_path, Kramdown::Document.new(text, input: 'GFM', coderay_line_numbers: nil).to_html)      ## use Kramdown to parse Github-flavored-markdown (GFM)
+  wrap_file_in_html(html_path)    
 end
 
-def make_nice_html(source_md_path, tar_html_path)    
-  text = File.read(source_md_path)
-  File.write(tar_html_path, Kramdown::Document.new(text, input: 'GFM', coderay_line_numbers: nil).to_html)
-  wrap_file_in_html(tar_html_path)      
-end
-
-#2 build blog_src/index.md
-  #2.1 build list of posts, each with title, url, created_At
-def get_internal_blog_posts_list
-  puts __method__
-  blog_posts = get_blog_mds_paths
-
-  blog_posts.map! do |path| 
-    hash = parse_firstline_comment_as_hash(path)
-    next if (hash['draft'] || path=='blog_src/index.md')
-    hash['title'] = hash['title'] || path.sub('blog_src/', '').sub('.md','')
-    hash['url']   = hash['url']   || path.sub('blog_src/', '').sub('.md','.html')
-    hash
-  end
-
-  blog_posts.compact! #remove nils
-end
-
-def get_blog_index_list
-  puts __method__
-  res = get_internal_blog_posts_list + get_external_links_list
-  res.sort_by {|obj| obj['created_at'] || '-2000' }.reverse
-end
-
-def build_blog_index_md
-  puts __method__
-  content = "## Sella's Blog \n"
-  content +="<!-- this file is auto-created. -->\n\n"
-  content += "###### now that's what I'm talking about\n"
-  get_blog_index_list.each do |item| 
-    title = item['title']      || item[:title]
-    url   = item['url']        || item[:url]
-    created_at = item['created_at'] || item[:created_at]
-    content+="* [#{title}](#{url})"
-    content+="<span class='created_at'>#{created_at}</span>\n" if created_at
-  end
-
-  File.write('blog_src/index.md', content)   
-end
-
-def build_blog_html_files
-  puts __method__
-  paths = get_blog_mds_paths
-  paths.each do |path| 
-    html_path = path.sub('.md','.html').sub('_src','');
-    make_nice_html(path, html_path)    
-  end
-end
-
-build_basic_pages
-build_blog_index_md
-build_blog_html_files
-
+puts "Done!"
+  
